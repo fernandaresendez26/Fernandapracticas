b7c75d537033955cfd2d0f2a481f2689e63472a3
fernandaresendez26 <mfresendezs@outlook.com>
2025-07-09 00:14:36 -0600

    Add files via upload
    practica1

---
+++ MariafernandaR_practica1.py
@@ -0,0 +1,45 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+print("distancia entre dos puntos")
+distancia entre dos puntos
+input("lo solucionaremos con dos metodos")
+lo solucionaremos con dos metodos
+''
+>>> input("distancia manhatan")
+distancia manhatan
+''
+>>> 
+>>> x1,y1= 2,2
+>>> x2,y2= 4,6
+>>> print(x1,y1)
+2 2
+>>> print(x2,y2)
+4 6
+>>> distancia= (x2-x1)+(y2-y1)
+>>> print(distancia)
+6
+>>> input("distancia eucladiana")
+distancia eucladiana
+''
+>>> x1,y1= 2,2
+>>> x2,y2= 4,6
+>>> distancia= (x2-x1)
+>>> print(distancia)
+2
+>>> distancia= (y2-y1)
+>>> print(distancia)
+4
+>>> numero= 2**2
+>>> print(numero)
+4
+>>> numero= 4**2
+>>> print(numero)
+16
+>>> distancia= (4+16)
+>>> print(distancia)
+20
+>>> import math
+>>> numero= 20
+>>> raiz= math.sqrt(numero)
+>>> print("raiz cuadrada:",raiz)
+raiz cuadrada: 4.47213595499958
